be3cf02cedaf58ca5d9e3dec3c86ceeaca6cd168
George <118970777+GergT@users.noreply.github.com>
2025-04-10 09:59:06 +0100

    Update entActions.js

--- WEB PROG COURSEWORK/entActions.js
+++ WEB PROG COURSEWORK/entActions.js
@@ -1,6 +1,6 @@
-
 document.getElementById("ear").addEventListener("click",function(){
     let div = document.getElementById("earInfo")
+    getLocation(1);
     if (div.style.maxHeight) {
         div.style.maxHeight = null;
     } else {
@@ -11,6 +11,7 @@ document.getElementById("ear").addEventListener("click",function(){
 
 document.getElementById("nose").addEventListener("click",function(){
     let div = document.getElementById("noseInfo")
+    getLocation(2);
     if (div.style.maxHeight) {
         div.style.maxHeight = null;
     } else {
@@ -21,6 +22,7 @@ document.getElementById("nose").addEventListener("click",function(){
 
 document.getElementById("throat").addEventListener("click",function(){
     let div = document.getElementById("throatInfo")
+    getLocation(3);
     if (div.style.maxHeight) {
         div.style.maxHeight = null;
     } else {
@@ -31,6 +33,7 @@ document.getElementById("throat").addEventListener("click",function(){
 
 document.getElementById("pediatric").addEventListener("click",function(){
     let div = document.getElementById("pediatricInfo")
+    getLocation(4);
     if (div.style.maxHeight) {
         div.style.maxHeight = null;
     } else {
@@ -41,6 +44,7 @@ document.getElementById("pediatric").addEventListener("click",function(){
 
 document.getElementById("allergy").addEventListener("click",function(){
     let div = document.getElementById("allergyInfo")
+    getLocation(5);
     if (div.style.maxHeight) {
         div.style.maxHeight = null;
     } else {
@@ -51,10 +55,12 @@ document.getElementById("allergy").addEventListener("click",function(){
 
 document.getElementById("headneck").addEventListener("click",function(){
     let div = document.getElementById("surgeryInfo")
+    getLocation(6);
     if (div.style.maxHeight) {
         div.style.maxHeight = null;
     } else {
-        closeAll()
+        
+        closeAll();
         div.style.maxHeight = div.scrollHeight + "px";
     }
 })
@@ -105,8 +111,7 @@ function sortTable(option,tableNo) {
         column = 1;  // Price column index
     }
 
-    let newID = "consultantsTable" + tableNo;
-    console.log(newID);
+    const newID = "consultantsTable" + tableNo;
     let table = document.getElementById(newID);
     let rows = Array.from(table.rows).slice(1); // Skip the header row
 
@@ -124,4 +129,66 @@ function sortTable(option,tableNo) {
 
     // Append the sorted rows back to the table
     rows.forEach(row => table.appendChild(row));
+}
+
+
+function getLocation(currentTable) {
+    const backup = {lat: 52.843962, lng: -1.295724} // my backup location in the east midlands incase the user does not share their gps
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                initMap(position, currentTable,"NORMAL");
+            },
+            (error) => {
+                console.error("Geolocation error:", error);
+                initMap(backup, currentTable,"IRREG");
+
+            }
+        );
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+        initMap(backup, currentTable,"IRREG");
+    }
+}
+
+
+function initMap(userPosition,tableNo,status) {
+
+    let origin;
+    console.log(status == "NORMAL");
+    if (status == "NORMAL"){
+        origin = { lat: userPosition.coords.latitude, lng: userPosition.coords.longitude };
+    }
+    else{
+        origin = {lat: userPosition.lat, lng: userPosition.lng}
+    }
+    let destinations = [];
+    let table = document.getElementById("consultantsTable"+tableNo);
+    let rows = Array.from(table.rows).slice(1);
+    
+    rows.forEach((row) =>{
+        const lat = parseFloat(row.dataset.lat);
+        const long = parseFloat(row.dataset.long);
+
+        
+        destinations.push({
+            lat: lat, 
+            lng: long
+        });
+    });
+  
+    const map = new google.maps.Map(document.getElementById("map"+tableNo), {
+      center: origin,
+      zoom: 9,
+    });
+    
+    if (status == 'NORMAL'){
+        new google.maps.Marker({ position: origin, map, label: "YOU" });
+    }
+    console.log(destinations)
+    
+    for (let i = 0;i<rows.length;i++){
+        new google.maps.Marker({ position: destinations[i], map, label: rows[i].cells[0].innerText });
+    }
 }
